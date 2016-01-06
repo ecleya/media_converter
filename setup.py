00dde39f83ff64e2cbc45216d897615da3fe8841
ecleya <ecleya@smartstudy.co.kr>
2016-01-06 17:49:37 +0900

    마이너 버전 업.

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
+from media_converter import version, PROJECT
 from setuptools import setup, find_packages
 
 
 MODULE_NAME = 'media_converter'
 PACKAGE_DATA = list()
-
-__version__ = version = '0.0.1'
-__project__ = PROJECT = 'media_converter'
-__author__ = AUTHOR = "Kiheon Choi <ecleya@smartstudy.co.kr>"
-
 
 setup(
     name=PROJECT,
