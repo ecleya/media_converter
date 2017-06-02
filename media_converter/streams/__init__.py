a3abcb73d024c27e9d925c03e80c046208083554
ecleya <ecleya@gmail.com>
2017-06-03 02:16:56 +0900

    fix flake8

--- media_converter/streams/__init__.py
+++ media_converter/streams/__init__.py
@@ -1,6 +1,8 @@
-from media_converter.streams.instream import Instream, VideoInstream, AudioInstream, SilentAudioInstream, ImageSequenceInstream, ImageInstream, SubtitleInstream
+from media_converter.streams.instream import Instream, VideoInstream, ImageSequenceInstream, ImageInstream
+from media_converter.streams.instream import AudioInstream, SilentAudioInstream, SubtitleInstream
 from media_converter.streams.outstream import Outstream, VideoOutstream, AudioOutstream, SubtitleOutstream
 
 
 __all__ = ['Outstream', 'VideoOutstream', 'AudioOutstream', 'SubtitleOutstream',
-           'Instream', 'VideoInstream', 'AudioInstream', 'SilentAudioInstream', 'SubtitleInstream', 'ImageSequenceInstream', 'ImageInstream']
+           'Instream', 'VideoInstream', 'AudioInstream', 'SilentAudioInstream',
+           'SubtitleInstream', 'ImageSequenceInstream', 'ImageInstream']
